7d60f0096a1b16dc9f7b16d8fff25decb69f0c08
maddenlockin <maddenlockin@gmail.com>
2021-06-30 17:23:41 -0700

    file structure laid out and routes and links established

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+    Link,
+    Redirect
+} from "react-router-dom";
+import TodosPage from './TodosPage';
+import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
+import HomePage from './HomePage';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { token: localStorage.getItem('TOKEN') }
+
+  handleTokenChange = (myToken) => {
+    this.setState({ token: myToken });
+    localStorage.setItem('TOKEN', myToken);
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/todos">To-do List</Link>
+            <button onClick={() => this.handleTokenChange('')}>logout</button>
+          </ul>
+          <Switch>
+            <Route 
+            exact path='/' 
+            render={(routerProps) => <HomePage  
+                {...routerProps} />} 
+              />
+            <Route 
+            exact path='/login' 
+            render={(routerProps) => <LoginPage 
+                {...routerProps} />} 
+              />
+            <Route 
+            exact path='/signup' 
+              render={(routerProps) => <SignUpPage 
+                {...routerProps}/>} 
+              />
+          <Route 
+            exact path='/todos' 
+            render={(routerProps) =>
+              this.state.token 
+                ? <TodosPage {...routerProps} token={this.state.token} /> 
+                : <Redirect to="/" />
+              } 
+          />
+
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
